18a109958adaa8c8cc3ec69a9d529266d2727d6d
Tigran Gasparyan <tigrangasparyan777@gmail.com>
2022-03-15 16:36:32 -0700

    naxagidz v1.0

---
+++ blocks/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Նախագծային Աշխատանք</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    
+<div class=" d-flex   align-items-center p-3 px-md-4 s  header">
+        
+        <nav class="  my-2 my-md-0 mr-md-3">
+            <a class="header_link" href="index.php"><strong>Home</strong></a><br>
+            <a class="header_link1" href="nor.php"><strong>ժամանակակից մասնագիտություն</strong></a><br>
+            <a class="header_link2" href="hin.php"><strong>ծերացող մասնագիտություն</strong></a><br>
+            <a class="header_link3" href="durov.php"><strong>հաջողակ մարդու մի օրինակ</strong></a>
+        </nav>
+</div>
+    <br>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php require "blocks/header.php"?>
+
+<div class="welcome_div container "><h1 class="h1_welcome">Հաջողակ մարդու մի օրինակ </h1> 
+<h1 class="h1_welcomey2">Pavel Durov</h1> 
+    </div>
+    <img src="img/durov1.jpg" class="img">
+
+    <img src="img/durov3.jpg" class="img2">
+ 
+<br>
+    <p class="p"> Պավել Վալերիևիչ Դուրով (ծնվ. հոկտեմբերի 10, 1984, Լենինգրադ), ռուս ձեռնարկատեր, ծրագրավորող, դոլարային միլիարդատեր։
+     VKontakte սոցիալական ցանցի և համանուն ընկերության, միջպլատֆորմային մեսենջերի Telegram-ի և այլ նախագծերի ստեղծողներից մեկը։ VKontakte-ի նախկին գործադիր տնօրեն (2006-2014)։
+     </p>
+
+     <a href="durov.php" class="more">More</a>
+
+<br><br>
+     <div class="welcome_div container "><h1 class="h1_welcome">ժամանակակից մասնագիտություն  </h1> 
+    <h1 class="h1_welcomey2">Ծրագրավորում</h1> 
+    </div>
+
+    <img src="img/coding1.jpg" class="img3">
+    <img src="img/coding2.jpg" class="img4">
+
+
+    <p class="p">
+    Ծրագրավորումը համակարգչային ծրագրերի ստեղծման գործընթաց է: Ծրագրավորման լեզուների հիմնադիրներից մեկի՝ Նիկլաուս Վիրտի խոսքերով՝
+    «Ծրագրեր = ալգորիթմներ + տվյալների կառուցվածքներ»։
+    Ծրագրավորումը հիմնված է ծրագրավորման լեզուների օգտագործման վրա, որոնցում գրված են ծրագրերի սկզբնական տեքստերը: 
+    </p>
+
+    <a href="nor.php" class="more">More</a>
+
+<br><br>
+     <div class="welcome_div container "><h1 class="h1_welcome">ծերացող մասնագիտություն  </h1> 
+    <h1 class="h1_welcomey2">Телефонист</h1> 
+    </div>
+
+    <img src="img/telefonist1.jpg" class="img5">
+    <img src="img/telefonist2.jpg" class="img6">
+
+
+    <p class="p">
+    Հեռախոսային օպերատորն այն մարդն է (օպերատորը), ով կատարում է միացում հեռախոսակայանում, այսինքն՝ միացնում է հեռախոսային բաժանորդներին հեռախոսագծով զանգահարելիս:
+     Ռազմական գործերում հեռախոսավարը [զինվորական գրանցման մասնագիտություն է,
+     պաշտոն և անձ, ով ունի դրանք և գտնվում է աշխարհի այս կամ այն պետության զինված ուժերի ազդանշանային զորքերում ։ Կա նաև ավագ հեռախոսավար։ 
+    </p>
+
+    <a href="hin.php" class="more">More</a>
+
+
+
+<?php require "blocks/footer.php"?>
